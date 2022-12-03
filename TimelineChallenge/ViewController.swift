0d5b6ce3e5606c8a8bbc5c0420d3cd45a5b07dee
Elisa Camillo <elisa.camillo@mobile2you.com.br>
2022-12-03 15:36:04 -0300

    Initial Commit

---
+++ TimelineChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimelineChallenge
+//
+//  Created by Elisa Camillo on 03/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TimelineChallengeUITests/TimelineChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TimelineChallengeUITestsLaunchTests.swift
+//  TimelineChallengeUITests
+//
+//  Created by Elisa Camillo on 03/12/22.
+//
+
+import XCTest
+
+final class TimelineChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
